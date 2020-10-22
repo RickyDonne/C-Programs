22f23592599146813421c190c93da29568ce0a10
Ricky <debarshibiswas.jgm@gmail.com>
2020-10-22 22:45:26 +0530

    Create 10.c

---
+++ Flow Examples/10.c
@@ -0,0 +1,25 @@
+//
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    for (i = 1; i <= 10; ++i) {
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    return 0;
+}
+
+//
+#include <stdio.h>
+int main() {
+    int n, i, range;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    printf("Enter the range: ");
+    scanf("%d", &range);
+    for (i = 1; i <= range; ++i) {
+        printf("%d * %d = %d \n", n, i, n * i);
+    }
+    return 0;
+}

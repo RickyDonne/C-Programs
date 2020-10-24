d1f83d3a8d29470657b0d3ad4557a0eb91f11fdf
Ricky <debarshibiswas.jgm@gmail.com>
2020-10-24 18:53:53 +0530

    Create 12.c

---
+++ Flow Examples/12.c
@@ -0,0 +1,41 @@
+//Fibonacci Series up to n terms
+
+#include <stdio.h>
+int main() {
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; ++i) {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+
+    return 0;
+}
+
+
+//Fibonacci Series up to n terms
+
+#include <stdio.h>
+int main() {
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+
+    // displays the first two terms which is always 0 and 1
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    nextTerm = t1 + t2;
+
+    while (nextTerm <= n) {
+        printf("%d, ", nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+
+    return 0;
+}

a0167168e0926ab6fd523ce4ea963729f921f20e
Ricky <debarshibiswas.jgm@gmail.com>
2021-01-12 22:59:24 +0530

    Create MUFFINS3.c

---
+++ MUFFINS3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+     int n ,c=0;
+    while(t--)
+    {
+       
+        scanf("%d",&n);
+        c=(n/2)+1;
+        printf("%d\n",c);
+      
+    }
+	// your code goes here
+	return 0;
+}
+
